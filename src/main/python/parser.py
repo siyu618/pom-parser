3135c5f9925757d6e899e45b781174ec22f89a9a
tianzhiyu <tianzhiyu@bogon.didichuxing.com>
2018-12-12 22:24:23 +0800

    minor

--- src/main/python/parser.py
+++ src/main/python/parser.py
@@ -20,6 +20,7 @@ ARTIFACTORY_WEB_URL = "http:///xxxxxxx/artifactory/libs-release-local"
 TOKEN = "TOKNE#####"
 APIKEY = "APILKY####"
 HEADERS = {"token": TOKEN, "apikey": APIKEY}
+ENCODING = "UTF-8"
 
 
 def usage():
@@ -63,26 +64,47 @@ def test_if_has_release_in_artifactory_by_web(groupid, artifactid, version):
 
 def get_pom_module_version(inputfile):
     root = ET.parse(inputfile)
-    version = root.find("./project:version", NAMESPACE_MAP).text
     groupid = root.find("./project:groupId", NAMESPACE_MAP).text
     artifactid = root.find("./project:artifactId", NAMESPACE_MAP).text
     packing_filed = root.find("./project:packaging", NAMESPACE_MAP)
     packing = "jar"
     if packing_filed is not None:
         packing = packing_filed.text
+    version = None
+    version_filed = root.find("./project:version", NAMESPACE_MAP)
+    if version_filed is not None:
+        version = version_filed.text
+    else:
+        parent_filed = root.find("./project:parent", NAMESPACE_MAP)
+        parent_version_field = parent_filed.find("./project:version", NAMESPACE_MAP)
+        if parent_version_field is not None:
+            version = parent_version_field.text
+    if version is None:
+        raise RuntimeError("no version info in pom file %s " % inputfile)
     return {"version": version, "artifactId": artifactid, "groupId": groupid, "packing": packing}
     pass
 
 
-def remove_snapshot_pom_module_version(inputfiles=[]):
+def remove_snapshot_pom_module_version(inputfiles=[], file_info_map={}):
     for inputfile in inputfiles:
         root = ET.parse(inputfile)
-        version = root.find("./project:version", NAMESPACE_MAP).text
-        if version.endswith(SNAPSHOT_SUFFIX):
-            new_version = version[0:version.rfind(SNAPSHOT_SUFFIX)]
-            root.find("./project:version", NAMESPACE_MAP).text = new_version
-            # logging.info("rewrite file{} version from {} to {}", inputfile, version, new_version)
-            root.write(inputfile, default_namespace=NAMESPACE)
+        if root.find("./project:version", NAMESPACE_MAP) is not None:
+            version = root.find("./project:version", NAMESPACE_MAP).text
+            if version.endswith(SNAPSHOT_SUFFIX):
+                new_version = version[0:version.rfind(SNAPSHOT_SUFFIX)]
+                root.find("./project:version", NAMESPACE_MAP).text = new_version
+                # logging.info("rewrite file{} version from {} to {}", inputfile, version, new_version)
+                root.write(inputfile, encoding=ENCODING, default_namespace=NAMESPACE)
+        else:
+            parent_filed = root.find("./project:parent", NAMESPACE_MAP)
+            parent_version_field = parent_filed.find("./project:version", NAMESPACE_MAP)
+            version = parent_version_field.text
+            if version.endswith(SNAPSHOT_SUFFIX):
+                new_version = version[0:version.rfind(SNAPSHOT_SUFFIX)]
+                version_element = ET.SubElement(root.getroot(), '{' + NAMESPACE + '}version')
+                version_element.text = new_version
+                print "after :", root.find("./project:version", NAMESPACE_MAP).text
+                root.write(inputfile + '.new', encoding=ENCODING, default_namespace=NAMESPACE)
     pass
 
 
@@ -121,7 +143,7 @@ def test_if_has_release(file_info={}):
         # return test_if_has_release_in_artifactory(groupId, artifactId, version)
         return test_if_has_release_in_artifactory_by_web(groupId, artifactId, version)
     except Exception as e:
-        print "failed to get release info for file "
+        print "failed to get release info for file ", file_info
         return True
     return True
     pass
@@ -142,7 +164,7 @@ def get_file_who_has_no_release(files_with_snapshots=[], file_info_map={}):
         if file_name not in file_info_map:
             continue
         file_info = file_info_map.get(file_name)
-        if not test_if_has_release(file_info):
+        if test_if_has_release(file_info):
             res.append(file_name)
     return res
     pass
@@ -164,7 +186,7 @@ def check_and_rename(inputfiles=[]):
     # 4. backup
     do_backup_files(files_with_snapshots_with_no_release)
     # 5. remove snapshot
-    remove_snapshot_pom_module_version(files_with_snapshots_with_no_release)
+    remove_snapshot_pom_module_version(files_with_snapshots_with_no_release, file_info_map)
     pass
 
 
